5352f878de9a40a3454255103ad2241b7f90f376
agrawal-piyush <piyushagrawal826@gmail.com>
2023-01-05 17:14:52 +0530

    Added spinner and loading

--- src/components/News.js
+++ src/components/News.js
@@ -1,51 +1,55 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner'
 export class News extends Component {
   constructor(){
     
     super()
     this.state={
       articles:[],  
-      loading: false,
+      loading:false,
       page:1,
-      nxt:false
     }
   }
   
   
   async componentDidMount(){
-    let url ="https://newsapi.org/v2/top-headlines?country=in&apiKey=6ed7e1877c8d42a89b18825fcdbc68e1&page=1&pagesize=20"
+    let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=6ed7e1877c8d42a89b18825fcdbc68e1&page=1&pagesize=${this.props.pagesize}`
+    this.setState({loading:true})
     let data = await fetch(url)
     let parseData = await data.json()
     console.log(parseData)
-    this.setState({articles:parseData.articles, totalResults: parseData.totalResults})
+    this.setState({articles:parseData.articles, totalResults: parseData.totalResults,loading:false})
   }
   
   PREV= async ()=>{
-    let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=6ed7e1877c8d42a89b18825fcdbc68e1&page=${this.state.page-1}&pagesize=20`
+    
+    let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=6ed7e1877c8d42a89b18825fcdbc68e1&page=${this.state.page-1}&pagesize=${this.props.pagesize}`
+    this.setState({loading:true})
     let data = await fetch(url)
     let parseData = await data.json()
-    console.log(parseData)
     
     this.setState({
       page: this.state.page-1,
-      articles:parseData.articles
+      articles:parseData.articles,
+      loading:false
     })}
   
   NEXT= async ()=>{
-    if (this.state.page + 1>Math.ceil(this.state.totalResults/20))
+    if (this.state.page + 1<Math.ceil(this.state.totalResults/this.props.pagesize))
     {
 
-    }
-    else{
-      let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=6ed7e1877c8d42a89b18825fcdbc68e1&page=${this.state.page+1}&pagesize=20`
+    
+    
+      let url =`https://newsapi.org/v2/top-headlines?country=in&apiKey=6ed7e1877c8d42a89b18825fcdbc68e1&page=${this.state.page+1}&pagesize=${this.props.pagesize}`
+      this.setState({loading:true})
       let data = await fetch(url)
       let parseData = await data.json()
-      console.log(parseData)
-      
+
       this.setState({
         page: this.state.page+1,
-        articles:parseData.articles
+        articles:parseData.articles,
+        loading:false
        
          })}
   }
@@ -53,11 +57,12 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-       
-        <h2>Top HeadLines</h2>
+       <h1 className="text-center">Top HeadLines</h1>
+       {this.state.loading && <Spinner/>}
+               
        <div className="row">
         
-       {this.state.articles.map((element)=>{
+       {!this.state.loading && this.state.articles.map((element)=>{
          return <div className="col-md-4" key ={element.url}>
         <Newsitem   title={element.title?element.title.slice(0,45):"News"} description={element.description?element.description.slice(0,88):"click below to know more"} imageUrl ={element.urlToImage?element.urlToImage:"https://static.toiimg.com/thumb/imgsize-37494,msid-96724015,width-400,resizemode-4/96724015.jpg"} newsUrl={element.url}/>
         </div>
@@ -67,7 +72,7 @@ export class News extends Component {
        )}
        <div className="contianer d-flex justify-content-between">
         <button type='button' disabled = {this.state.page<=1} className='btn btn-dark'onClick={this.PREV}> &larr; Previous </button>
-        <button type='button'disabled={this.state.page + 1>Math.ceil(this.state.totalResults/20)} className='btn btn-dark' onClick={this.NEXT}> Next &rarr; </button>
+        <button type='button'disabled={this.state.page + 1>Math.ceil(this.state.totalResults/this.props.pagesize)} className='btn btn-dark' onClick={this.NEXT}> Next &rarr; </button>
        
 
        </div>
