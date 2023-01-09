538c318c114a1a1993a2e2dcb175b3a37f6b2b10
agrawal-piyush <piyushagrawal826@gmail.com>
2023-01-09 11:54:07 +0530

    adding source,date and author

--- src/components/News.js
+++ src/components/News.js
@@ -78,7 +78,9 @@ export class News extends Component {
         
        {!this.state.loading && this.state.articles.map((element)=>{
          return <div className="col-md-4" key ={element.url}>
-        <Newsitem   title={element.title?element.title.slice(0,45):"News"} description={element.description?element.description.slice(0,88):"click below to know more"} imageUrl ={element.urlToImage?element.urlToImage:"https://static.toiimg.com/thumb/imgsize-37494,msid-96724015,width-400,resizemode-4/96724015.jpg"} newsUrl={element.url}/>
+        <Newsitem   title={element.title?element.title.slice(0,45):"News"} description={element.description?element.description.slice(0,88):"click below to know more"} imageUrl ={element.urlToImage?element.urlToImage:"https://static.toiimg.com/thumb/imgsize-37494,msid-96724015,width-400,resizemode-4/96724015.jpg"} newsUrl={element.url}
+        author ={element.author} date = {element.publishedAt} source ={element.source.name}
+        />
         </div>
 
     
